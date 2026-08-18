2b5c7863d44eb076a1f467776307c496b38b82b0
Cauê Ripka <2007.caue.tj@gmail.com>
2026-08-18 17:05:31 -0300

    parte um

---
+++ cadastrar_cliente.php
@@ -0,0 +1,59 @@
+<?php 
+$erro = false;
+if(count($_POST) > 0){
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $nascimento = $_POST['nascimento'];
+
+    if(empty($nome) || strlen($nome) < 3 || strlen($nome) > 100){
+        echo "O campo nome deve ser preenchido corretamente";
+    }
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "O campo E-mail deve ser preenchido corretamente";
+    }if($erro){
+        echo "<p><b>$erro</b></p>";
+    }if(strlen($telefone) != 11){
+        echo "Preencha o campo telefone corretamente no padrão (48) 99999-9999";
+    }if(strlen($nascimento) < 8 || strlen($nascimento) > 8){
+        echo "Preencha a data de nascimento corretamnete no padrão dd/mm/aaaa";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <a href="clientes.php">Voltar para a lista</a>
+    </nav><br> 
+     <h1>Preencha os dados do login</h1><br>
+    <form action="" method="post">
+        <div>
+            <label for="">Nome:</label><span class="error">*</span>
+            <input required name="nome" type="text"><br>
+        </div>
+        <div>
+            <label for="">E-mail:</label><span class="error">*</span>
+            <input required name="email" type="text"><br>
+        </div>
+        <div>
+            <label for="">Telefone:</label>
+            <input name="telefone" type="text"><br>
+        </div>
+        <div>
+            <label for="">Data de nascimento:</label>
+            <input name="nascimento" type="text"><br>
+        </div>
+        <button type="button" onclick="window.location.href='./prod/financas.php'">
+            Enviar dados
+        </button>
+    </form>
+</body>
+</html>
